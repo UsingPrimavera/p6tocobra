e1b48b95066af90da5cfe37521beab1bc23da52f
Barrie Callender <barrie@callenb.org>
2016-03-29 11:24:36 +0100

    <NullJob> provides the right name

---
+++ test/p3eapi/NullJobTest.java
@@ -0,0 +1,20 @@
+package p3eapi;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class NullJobTest {
+
+	public NullJobTest() {
+	}
+
+	@Test
+	public void testHasRightName() {
+
+		String[] args ={"username","c:\\Program Files\\Deltek\\Cobra\\p3eapi", "MyProjectId","s","true"};
+
+		NullJob job = new NullJob(new Parameters(args));
+		assertEquals("Null", job.name());
+		assertEquals(Job.class, job.getClass().getSuperclass());
+	}
+}
