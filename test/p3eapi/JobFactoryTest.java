196e3d38854c501db599f9ab9c0d9c8d374f035d
Barrie Callender <barrie@callenb.org>
2016-03-29 11:19:30 +0100

    <Jobfactory> returns the right instance objects

---
+++ test/p3eapi/JobFactoryTest.java
@@ -0,0 +1,40 @@
+package p3eapi;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class JobFactoryTest {
+
+	public JobFactoryTest() {
+	}
+
+	@Test
+	public void testCreatesRunJob() {
+
+		String[] args ={"username","password","c:\\Program Files\\Deltek\\Cobra\\p3eapi", "MyProjectId","s"};
+
+		Job runJob = JobFactory.getJob(new Parameters(args));
+		assertEquals("Run", runJob.name());
+		assertEquals(Job.class, runJob.getClass().getSuperclass());
+	}
+
+	@Test
+	public void testCreatesTestConnectionJob() {
+
+		String[] args ={"username","password","c:\\Program Files\\Deltek\\Cobra\\p3eapi", "TestConnection","s","true"};
+
+		Job testConnectionJob = JobFactory.getJob(new Parameters(args));
+		assertEquals("TestConnection", testConnectionJob.name());
+		assertEquals(Job.class, testConnectionJob.getClass().getSuperclass());
+	}
+
+	@Test
+	public void testCreatesNullJob() {
+
+		String[] args ={"username","c:\\Program Files\\Deltek\\Cobra\\p3eapi", "TestConnection","s","true"};
+
+		Job nullJob = JobFactory.getJob(new Parameters(args));
+		assertEquals("Null", nullJob.name());
+		assertEquals(Job.class,nullJob.getClass().getSuperclass());
+	}
+}
