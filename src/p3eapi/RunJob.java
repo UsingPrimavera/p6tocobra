03c0a64efdce52434542b189aa498cf8c0509ee5
Barrie Callender <barrie@callenb.org>
2017-11-28 20:06:37 +0000

    started a skeleton of main processing
    This is not well thought out so I have left it whilst I address other
    problems.  When I get back to RunJob.java I hope to be using git better.

--- src/p3eapi/RunJob.java
+++ src/p3eapi/RunJob.java
@@ -1,9 +1,12 @@
 package p3eapi;
 
+import com.primavera.integration.client.bo.BOIterator;
+import com.primavera.integration.client.bo.object.ResourceAssignment;
+
 public class RunJob extends Job {
 
-    private Parameters params = null;
-    private P6Connection p6Conn = null;
+  private Parameters params = null;
+  private P6Connection p6Conn = null;
 
 	public RunJob(Parameters params, P6Connection p6Conn) {
 		super(params, p6Conn);
@@ -14,15 +17,38 @@ public class RunJob extends Job {
 	public String name() { return "Run"; }
 
 	public Boolean run() {
-		return false;
+    Boolean retval = false;
+
+    // connect to P6 database
+    this.p6Conn.login(this.params.username(), this.params.password());
+    if ( this.p6Conn.isLoggedIn()) {
+    //   Determine if Project or Baseline
+      IProject project = this.p6Conn.getProjectOrBaseline(params.projectId());
+
+    //   Get all Resource assignments
+
+    //   for each Resource assignments
+
+    //    load the resource assignment spread
+
+    //    For Each Resource Assignment Spread
+
+    //      Extract data and ssave to csv
+    }
+		return retval;
 	}
 
-    public Boolean isError() { return true;}
+  public Boolean isError() { return true;}
 
-    public String message() {return "Nothing";}
+  public String message() {return "Nothing";}
 
-    public String getLogfilename() {
-        return params.pathName() + "\\p3eapiSpread.log";
-    }
+  public String getLogfilename() {
+    return params.pathName() + "\\p3eapiSpread.log";
+  }
+
+  public String getCSVfilename() {
+    return params.pathName() + "\\" + params.projectId() + ".csv";
+  }
+
 
 }
