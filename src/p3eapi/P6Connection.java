373b2a82c07ff226d99ace8986c5912a6b91a322
Barrie Callender <barrie@callenb.org>
2017-11-28 20:11:00 +0000

    added code to work out if it is a project or baseline

--- src/p3eapi/P6Connection.java
+++ src/p3eapi/P6Connection.java
@@ -1,8 +1,16 @@
 package p3eapi;
 
 import java.util.Hashtable;
-
 import java.util.logging.Logger;
+
+import com.primavera.integration.client.GlobalObjectManager;
+import com.primavera.integration.client.EnterpriseLoadManager;
+import com.primavera.integration.client.bo.BOIterator;
+import com.primavera.integration.client.bo.object.Project;
+import com.primavera.integration.client.bo.object.BaselineProject;
+import com.primavera.ServerException;
+import com.primavera.integration.network.NetworkException;
+import com.primavera.integration.client.bo.BusinessObjectException;
 
 /**
  * Holds all the information about how to connect to the Primavera API.
@@ -12,14 +20,14 @@ import java.util.logging.Logger;
 class P6Connection
 {
   private static Logger logger = Logger.getLogger(P6Connection.class.getName());
-  
+
   private Hashtable<String,Integer> modeMap = new Hashtable<String,Integer>();
 
   public static final String LOCAL_MODE = "local";
   public static final String STANDARD_MODE = "standard";
   public static final String SSL_MODE = "ssl";
   public static final String COMPRESSION_MODE = "compression";
-  
+
   private IRmiUrl p6rmiurl = null;
   private ISession p6session = null;
   private boolean isLoggedIn = false;
@@ -48,7 +56,7 @@ class P6Connection
     // open the properties file and read in the variables
 
     Configuration props = new Configuration();
-    
+
     logger.info("property mode=" + props.getProperty("mode",LOCAL_MODE));
     setCallingMode(props.getProperty("mode",LOCAL_MODE));
     setHostname(props.getProperty("hostname","http://localhost"));
@@ -79,7 +87,7 @@ class P6Connection
     else {
       iCallingMode = (Integer)modeMap.get(STANDARD_MODE);
     }
-    
+
     return getCallingMode();
   }
 
@@ -147,6 +155,43 @@ class P6Connection
 	  setIsLoggedIn(result);
   }
 
+  public IProject getProjectOrBaseline(String projectId) {
+    IProject project = null;
+    GlobalObjectManager gom = p6session.getGlobalObjectManager();
+    BOIterator<Project> boiProject = null;
+
+    try {
+      boiProject = gom.loadProjects(new String[] { "ObjectId", "Id", "Name" },
+          "Id = '" + projectId + "'", null );
+
+    // Are we dealing with a project?
+    if (boiProject.hasNext()) {
+      project = new P6Project(boiProject.next());
+    }
+    else {
+      EnterpriseLoadManager elm = p6session.getEnterpriseLoadManager();
+      BOIterator <BaselineProject> boiBaseline = elm.loadBaselineProjects(new String[] { "ObjectId", "Id", "Name" },
+          "Id = '" + projectId + "'", null );
+      if (boiBaseline.hasNext()) {
+        project = new P6Baseline(boiBaseline.next());
+      }
+    }
+  }
+  catch (ServerException ex) {
+    logger.info("Primavera ServerException error determining project or baseline");
+    logger.info(ex.toString());
+  }
+  catch (NetworkException ex) {
+    logger.info("Primavera NetworkException error determining project or baseline");
+    logger.info(ex.toString());
+  }
+  catch (BusinessObjectException ex) {
+    logger.info("Primavera BusinessObjectException error determining project or baseline");
+    logger.info(ex.toString());
+  }
+    return project;
+  }
+
   private void setIsLoggedIn(boolean bool) {
 	  this.isLoggedIn = bool;
   }
@@ -154,6 +199,6 @@ class P6Connection
   public boolean isLoggedIn() {
 	  return this.isLoggedIn;
   }
-	
+
 
 }
