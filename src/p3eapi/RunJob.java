8771fb6b1cb66628cc10effb272935d814f7a6c2
Barrie Callender <barrie@callenb.org>
2018-03-22 21:25:49 +0000

    RunJob has compiled code meant to extract data but not yet written to file. I plan to do the file writing then refactor. I may also add tests for this but most of it is making use of the P6 Java API so need to think of a valid test which tests the data coming out and not the external API.

--- src/p3eapi/RunJob.java
+++ src/p3eapi/RunJob.java
@@ -1,24 +1,40 @@
 package p3eapi;
 
+import com.primavera.common.value.BeginDate;
+import com.primavera.common.value.EndDate;
 import com.primavera.integration.client.bo.BOIterator;
 import com.primavera.integration.client.bo.object.ResourceAssignment;
+import com.primavera.ServerException;
+import com.primavera.integration.network.NetworkException;
+import com.primavera.integration.client.bo.BusinessObjectException;
+import java.util.logging.Logger;
+import java.util.Iterator;
+import com.primavera.common.value.spread.ResourceAssignmentSpread;
+import com.primavera.common.value.spread.ResourceAssignmentSpreadPeriod;
+import com.primavera.common.value.spread.SpreadFieldNotFoundException;
+import com.primavera.integration.client.bo.enm.SpreadPeriodType;
+import com.primavera.common.value.spread.SpreadPeriod;
+
 
 public class RunJob extends Job {
 
+  private static Logger logger = Logger.getLogger(RunJob.class.getName());
   private Parameters params = null;
   private P6Connection p6Conn = null;
   private String[] resourceAssignmentFields = null;
+  private String[] spreadFields = null;
 
 	public RunJob(Parameters params, P6Connection p6Conn) {
 		super(params, p6Conn);
         this.params = params;
         this.p6Conn = p6Conn;
-        if (this.params.datesource()=="B") {
-          this.resourceAssignmentFields = ["ObjectId","PlannedStartDate","PlannedFinishDate"];
+        if (this.params.dateSource()=="B") {
+          this.resourceAssignmentFields = new String[]{"ObjectId","PlannedStartDate","PlannedFinishDate"};
+          this.spreadFields = new String[]{"RemainingUnits", "RemainingCost", "PlannedUnits", "PlannedCost"};
         }
         else {
-          this.resourceAssignmentFields = ["ObjectId","StartDate","FinishDate"];
-
+          this.resourceAssignmentFields = new String[]{"ObjectId","StartDate","FinishDate"};
+          this.spreadFields = new String[]{"RemainingUnits", "RemainingCost", "AtCompletionUnits", "AtCompletionCost"};
         }
 	}
 
@@ -33,10 +49,42 @@ public class RunJob extends Job {
     //   Determine if Project or Baseline
       IProject project = this.p6Conn.getProjectOrBaseline(params.projectId());
       try {
+
+        ResourceAssignment resAss = null;
+        ResourceAssignment loadedResAss = null;
+        ResourceAssignmentSpread resAssSpread = null;
+        ResourceAssignmentSpreadPeriod resAssSpreadPeriod = null;
+        BeginDate startDate = null;
+        EndDate finishDate = null;
+        Iterator<SpreadPeriod> boiResAssSpread = null;
+
         BOIterator<ResourceAssignment> boiResAss = project.getResourceAssignments(this.resourceAssignmentFields);
+
         while (boiResAss.hasNext()) {
 
+          resAss = boiResAss.next();
           //    load the resource assignment spread
+          if (this.params.dateSource()=="B") {
+            startDate = resAss.getPlannedStartDate();
+            finishDate = resAss.getPlannedFinishDate();
+          }
+          else {
+            startDate = resAss.getStartDate();
+            finishDate = resAss.getFinishDate();
+          }
+
+          loadedResAss = ResourceAssignment.loadWithLiveSpread(
+            this.p6Conn.getSession(),
+            this.resourceAssignmentFields,
+            resAss.getObjectId(),
+            this.spreadFields,
+            SpreadPeriodType.DAY,
+            startDate,
+            finishDate,
+            false);
+
+          resAssSpread = loadedResAss.getResourceAssignmentSpread();
+          boiResAssSpread = resAssSpread.getSpreadIterator(false);
 
           //    For Each Resource Assignment Spread
 
@@ -46,6 +94,14 @@ public class RunJob extends Job {
       }
       catch (ServerException ex) {
           logger.info("Primavera ServerException error retrieving iterating over Resource Assignments");
+          logger.info(ex.toString());
+      }
+      catch (BusinessObjectException ex) {
+          logger.info("Primavera BusinessObjectException error retrieving iterating over Resource Assignments");
+          logger.info(ex.toString());
+      }
+      catch (NetworkException ex) {
+          logger.info("Primavera NetworkException error retrieving iterating over Resource Assignments");
           logger.info(ex.toString());
       }
     }
